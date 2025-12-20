43c5c702e9c5548208ffed6b9562daf1b273db04
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-20 10:30:06 +0100

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/dev.bragefuglseth.Fretboard.svg
 export DESKTOP=/usr/share/applications/dev.bragefuglseth.Fretboard.desktop
 export DEPLOY_OPENGL=1
-export STARTUPWMCLASS=fretboard
 export GTK_CLASS_FIX=1
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
